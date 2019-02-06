ebadd11ffc355e0c4c00b669a1fb435865ca1d87
Luis Yazmani Chairez Aguilera <luigi_71@hotmail.com>
2019-02-05 22:38:08 -0600

    Se agrega FixController

---
+++ WebApplicationGithub/Controllers/FixController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationGithub.Controllers
+{
+    public class FixController : Controller
+    {
+        // GET: Fix
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
